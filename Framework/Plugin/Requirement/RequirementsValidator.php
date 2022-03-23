920bee85e71152fc4fe2df021fc239aaf37fa774
xrogers <xrogers@gmail.com>
2022-03-23 15:15:47 +0100

    Support aliased versions of core packages etc.

--- Framework/Plugin/Requirement/RequirementsValidator.php
+++ Framework/Plugin/Requirement/RequirementsValidator.php
@@ -162,10 +162,12 @@ class RequirementsValidator
                 continue;
             }
 
+            $version = $package->getExtra()['branch-alias'][$package->getVersion()] ?? $package->getVersion();
+
             $pluginDependencies['require'] = $this->checkRequirement(
                 $pluginDependencies['require'],
                 $package->getName(),
-                new Constraint('==', $package->getVersion()),
+                new Constraint('==', $version),
                 $exceptionStack
             );
 
@@ -173,7 +175,7 @@ class RequirementsValidator
                 $pluginDependencies['conflict'],
                 $this->pluginComposer->getPackage()->getName(),
                 $package->getName(),
-                new Constraint('==', $package->getVersion()),
+                new Constraint('==', $version),
                 $exceptionStack
             );
 
